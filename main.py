87cf7a3e02b957581b3633f4a9d9a2bd1457ed07
RandiSPB <swat2299@gmail.com>
2019-04-17 01:10:42 +0300

    Adding 2 buttons and add msgbox

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
-from PyQt5.QtWidgets import(QWidget, QToolTip,
-    QPushButton, QApplication)
-from PyQt5.QtGui import QIcon
-
+from PyQt5.QtWidgets import (QWidget, QToolTip,
+    QPushButton, QApplication, QMessageBox)
+from PyQt5.QtGui import QFont, QIcon
+from PyQt5.QtCore import QCoreApplication
 
 class App(QWidget):
     '''
@@ -21,10 +21,14 @@ class App(QWidget):
         '''
         Инициализирование объектов(юнитов) интерфейса
         '''
+        QToolTip.setFont(QFont('SansSerif', 10))
         self.setGeometry(300, 150, self.width, self.height)
         self.setWindowTitle('Осциллятор')
         self.setWindowIcon(QIcon('icon.png'))
-        btn1 = self.createbutton()
+        start_btn = self.createbutton('Начать моделирование')
+        start_btn.setToolTip('Кнопка начала отрисовки модели')
+        qbtn = self.createbutton('Выйти', 100, 150)
+        qbtn.clicked.connect(QCoreApplication.instance().quit)
         self.show()
 
     def createbutton(self, name='Кнопка', x=100, y=50):
@@ -35,6 +39,15 @@ class App(QWidget):
         btn.resize(btn.sizeHint())
         btn.move(x, y)
         return btn
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Завершение работы',
+            "Уверены что хотите закрыть", QMessageBox.Yes |
+            QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 app = QApplication(sys.argv)
 ex = App(960, 540)
